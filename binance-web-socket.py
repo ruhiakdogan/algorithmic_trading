c9c1a9d031c636d4b9d6320d53676909b526ad35
Ruhi Akdoğan <55963399+ruhiakdogan@users.noreply.github.com>
2022-02-23 22:18:17 +0300

    Update binance-web-socket.py

--- binance-web-socket.py
+++ binance-web-socket.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import websocket, json
 
 cc = "btcusdt"
@@ -7,18 +6,9 @@ socket = f"wss://stream.binance.com:9443/ws/{cc}@depth10"
 def on_message(ws, message):
     json_string = message
     a_json = json.loads(json_string)
-    # print(a_json)
-
-    df = pd.DataFrame.from_dict(a_json)
-    # print(df)
-    del df["lastUpdateId"]
-    df["bid_price"] = pd.to_numeric(df["bids"].str[0])
-    df["bid_qnt"] = pd.to_numeric(df["bids"].str[1])
-    df["ask_price"] = pd.to_numeric(df["asks"].str[0])
-    df["ask_qnt"] = pd.to_numeric(df["asks"].str[1])
-    del df["bids"]
-    del df["asks"]
-    print(df)
+    print(a_json)
+    print(a_json["bids"][0][0]) # best bid
+    print(a_json["asks"][0][0]) # best ask
 
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
